faacfba3e9fc4a7bf4f603d444d8223dede4906f
fguerral93 <guerrafelipe93@gmail.com>
2021-06-30 13:39:27 -0400

    Fin sección 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: []
 })
 export class Grafica1Component implements OnInit {
+  
+
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data1 = [
+    [10, 15, 40],
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
